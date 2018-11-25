5db4a084c79b34a5492cf9c69c79ab425c9fc33f
Mark Piccirilli <mpicc704@gmail.com>
2018-11-25 07:10:57 -0500

    new directory structure.  one line changed in views.py

---
+++ app/views.py
@@ -0,0 +1,19 @@
+#CS 361 Group 15
+#Lyft but for truck drivers
+
+from flask import Flask, render_template
+
+from app import app
+#app = Flask(__name__, static_folder='static')
+
+@app.route("/")
+def main():
+    return render_template('index.html');
+
+@app.route("/hello/<name>")
+def nameroute(name):
+    return "Hello " + name
+    
+if __name__ == "__main__":
+    #This is set for compabilitity with Cloud9
+    app.run(host='0.0.0.0', port=8080, debug=True)
